ffc898a21d1e77a0311e4ecef1aadf22b41417d3
asthyeon <asthyeon@gmail.com>
2023-08-04 01:36:26 +0900

    1954

---
+++ 1954_snailnumber/1954.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T + 1):
+    N = int(input())
+
+    # 빈 어레이 형성
+    arr = [[0] * N for _ in range(N)]
+
+    # 숫자를 담을 칸 형성
+
+
+    # 달팽이 만들기
+    while True:
+        # 오른쪽으로 쭉 이동
+        pass
